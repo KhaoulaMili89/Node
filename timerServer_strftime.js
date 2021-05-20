7ef1f0ef1cb671bc7aa087c116306013a380df00
KhaoulaMili89 <81110750+KhaoulaMili89@users.noreply.github.com>
2021-05-20 10:03:46 +0100

    Update timerServer_strftime.js

--- timerServer_strftime.js
+++ timerServer_strftime.js
@@ -1,18 +1,18 @@
-var  net  =  require ( "net" ) ;
-var  strftime  =  require ( "strftime" ) ;
+// Required modules
+var net = require("net");
+var strftime = require("strftime");
 
-// Arguments de l'utilisateur
-var  demandéPort  =  8080 ;
-if  ( processus . argv [ 2 ]  !==  null )  {
-	requestedPort  =  parseInt ( processus . argv [ 2 ] ,  10 ) ;
+// User Arguments
+var requestedPort = 8080;
+if (process.argv[2] !== undefined) {
+	requestedPort = parseInt(process.argv[2], 10);
 }
 
-// Nouveau serveur TCP
-var  serveur  =  net . createServer ( fonction ( socket )  {
-	prise . end ( strftime ( '% F% R' ,  nouvelle  date ( ) )  +  "\ n" ) ;  // https://github.com/samsonjs/strftime
-} ) ;
+// New TCP server
+var server = net.createServer(function(socket) {
+	socket.end(strftime('%F %R', new Date()) + "\n"); // https://github.com/samsonjs/strftime
+});
 
 
 
-serveur . écouter ( requiredPort ) ;
-
+server.listen(requestedPort);
