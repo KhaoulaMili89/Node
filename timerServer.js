3433836616b7ac09bd3c7fbab8b3d2412ebab363
KhaoulaMili89 <81110750+KhaoulaMili89@users.noreply.github.com>
2021-05-20 09:59:16 +0100

    Update timerServer.js

--- timerServer.js
+++ timerServer.js
@@ -1,39 +1,39 @@
-var  net  =  require ( "net" ) ;
+// Required modules
+var net = require("net");
 
-// Arguments de l'utilisateur
-var  demandéPort  =  8080 ;
-if  ( processus . argv [ 2 ]  ! ==  non défini )  {
-	requestedPort  =  parseInt ( processus . argv [ 2 ] ,  10 ) ;
+// User Arguments
+var requestedPort = 8080;
+if (process.argv[2] !== undefined) {
+	requestedPort = parseInt(process.argv[2], 10);
 }
 
-// Le mois, le jour, l'heure et la minute doivent être remplis de zéro à 2 entiers
-function  formatForDoubleIntegers ( valeur )  {
-	if  ( valeur . longueur  ==  1 )  {
-		valeur  =  "0"  +  valeur ;
+// Month, day, hour and minute must be zero-filled to 2 integers
+function formatForDoubleIntegers(value) {
+	if (value.length == 1) {
+		value = "0" + value;
 	}
-	 valeur de retour ;
+	return value;
 }
 
-// Nouveau serveur TCP
-var  serveur  =  net . createServer ( fonction ( socket )  {
-	var  currentDate  =  nouvelle  date ( ) ;
+// New TCP server
+var server = net.createServer(function(socket) {
+	var currentDate = new Date();
 
-	var  YYYY  =  date actuelle . getFullYear ( ) . toString ( ) ;
-	var  MM  =  formatForDoubleIntegers ( ( currentDate . getMonth ( )  +  1 ) . toString ( ) ) ;
-	var  DD  =  formatForDoubleIntegers ( currentDate . getDate ( ) . toString ( ) ) ;
-	var  HH  =  formatForDoubleIntegers ( currentDate . getHours ( ) . toString ( ) ) ;
-	var  mm  =  formatForDoubleIntegers ( currentDate . getMinutes ( ) . toString ( ) ) ;
+	var YYYY = currentDate.getFullYear().toString();
+	var MM = formatForDoubleIntegers((currentDate.getMonth() + 1).toString());
+	var DD = formatForDoubleIntegers(currentDate.getDate().toString());
+	var HH = formatForDoubleIntegers(currentDate.getHours().toString());
+	var mm = formatForDoubleIntegers(currentDate.getMinutes().toString());
 
-	var  dateToReturn  =  AAAA  +  "-"  +  MM  +  "-"  +  JJ  +  ""  +  HH  +  ":"  +  mm ;
+	var dateToReturn = YYYY + "-" + MM + "-" + DD + " " + HH + ":" + mm;
 
-	// socket.write (dateToReturn);
-	// console.log (dateToReturn);
+	// socket.write(dateToReturn);
+	// console.log(dateToReturn);
 
-	prise . end ( dateToReturn  +  "\ n" ) ;
-} ) ;
+	socket.end(dateToReturn + "\n");
+});
 
 
 
-serveur . écouter ( requiredPort ) ;
-
+server.listen(requestedPort);
 
