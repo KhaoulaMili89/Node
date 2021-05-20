aa66f13ccb45e4b6445539b0d59176a9ff7b1b19
KhaoulaMili89 <81110750+KhaoulaMili89@users.noreply.github.com>
2021-05-20 09:46:29 +0100

    Update my-first-io.js

--- my-first-io.js
+++ my-first-io.js
@@ -1,14 +1,14 @@
-var  fileName  =  processus . argv [ 2 ] ;
-var  fs  =  exiger ( 'fs' ) ;
-var  buf  =  null ;
-var  nbLines  =  null ;
-try  {
-	buf  =  fs . readFileSync ( fileName ,  'utf8' ) ;
-	var  buffStr  =  buf . toString ( ) ;
-	var  linesArr  =  buffStr . split ( '\ n' ) ;
-	nbLines  =  linesArr . longueur  - 1 ;  // -1 à cause de la ligne à la fin du fichier
-	console . log ( nbLines ) ;
+var fileName = process.argv[2];
+var fs = require('fs');
+var buf = null;
+var nbLines = null;
+try {
+	buf = fs.readFileSync(fileName, 'utf8');
+	var buffStr = buf.toString();
+	var linesArr = buffStr.split('\n');
+	nbLines = linesArr.length -1; // -1 because of line in file's end
+	console.log(nbLines);
 }
-catch  ( e )  {
-	console . log ( e ) ;
+catch (e) {
+	console.log(e);
 }
