a0338ce0ac19d2ca9a62931d47b7d386c576be2c
KhaoulaMili89 <81110750+KhaoulaMili89@users.noreply.github.com>
2021-05-20 09:50:10 +0100

    Update myFirstAsyncIo.js

--- myFirstAsyncIo.js
+++ myFirstAsyncIo.js
@@ -1,14 +1,13 @@
-var  fs  =  exiger ( 'fs' ) ;
-var  fileName  =  processus . argv [ 2 ] ;
+var fs = require('fs');
+var fileName = process.argv[2];
 
 
-fs . readFile ( fileName ,  'utf8' )
-  function f ( err ,  données )  {
-	if  ( err )  {
-		  err ;
+fs.readFile(fileName, 'utf8', function (err, data) {
+	if (err) {
+		throw err;
 	}
-	else  {
-		var  nbLines  =  données . toString ( ) . split ( '\ n' ) . longueur  - 1  ;
-		console . log ( nbLines ) ;
+	else {
+		var nbLines = data.toString().split('\n').length -1 ;
+		console.log(nbLines);
 	}
-} 
+});
