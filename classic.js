1813d78bcdd35f61857d55a8e42e8bddbb8a1d6f
KhaoulaMili89 <81110750+KhaoulaMili89@users.noreply.github.com>
2021-05-20 09:57:05 +0100

    Update classic.js

--- classic.js
+++ classic.js
@@ -1,50 +1,51 @@
-var  http  =  require ( "http" ) ;
-var  bufferList  =  require ( "bl" ) ;
+// modules
+var http = require("http");
+var bufferList = require("bl");
 
-var  urlsToGet  =  [ ] ;
-var  dataOrdered  =  [ ] ;
-var  indexLoaded  =  0 ;
-var  i ;
+var urlsToGet = [];
+var dataOrdered = [];
+var indexLoaded = 0;
+var i;
 
 
 
-function  getData ( index )  {
+function getData(index) {
 	// traitement
-	http . get ( urlsToGet [ index ] ,  fonction ( réponse )  {
-		réponse . pipe ( bufferList ( fonction ( err ,  données )  {
-			if  ( err )  {
-				console . erreur ( err ) ;
-			};
-			dataOrdered [ index ]  =  données . toString ( ) ;
-			indexLoaded ++ ;
+	http.get(urlsToGet[index], function(response) {
+		response.pipe(bufferList(function(err, data) {
+			if (err) {
+				return console.error(err);
+			}
+			dataOrdered[index] = data.toString();
+			indexLoaded++;
 
-			if  ( indexLoaded  ===  3 )  {
-				pour  ( var  i  =  0 ;  i  <  3 ;  i ++ )  {
-					console . log ( dataOrdered [ i ] ) ;
+			if (indexLoaded === 3) {
+				for (var i = 0; i < 3; i++) {
+					console.log(dataOrdered[i]);
 				}
 			}
-		} ) ) ;
+		}));
 
-	} ) . on ( 'erreur' ,  fonction ( e )  {
-		console . error ( "Got error:"  +  e . message ) ;
-	} ) ;
+	}).on('error', function(e) {
+		console.error("Got error: " + e.message);
+	});
 }
 
-// paramètres
-if  ( process . argv [ 2 ]  ===  undefined )  {
+// params
+if (process.argv[2] === undefined) {
 	// urls par défaut si pas de paramètres
-	urlsToGet  =  [ "http://faubourgsimone.paris/" ,  "http://proustib.at" ,  "http://usine.io" ] ;
+	urlsToGet = ["http://faubourgsimone.paris/", "http://proustib.at", "http://usine.io"];
 }
-else  {
+else {
 	// récupération des 3 urls passées en arguments
-	for  ( i  =  0 ;  i  <  process . argv . length  -  2 ;  i ++ )  {
-		if  ( process . argv [ i  +  2 ]  ! ==  indéfini )  {
-			urlsToGet [ i ]  =  processus . argv [ i  +  2 ] ;
+	for (i = 0; i < process.argv.length - 2; i++) {
+		if (process.argv[i + 2] !== undefined) {
+			urlsToGet[i] = process.argv[i + 2];
 		}
 	}
 }
 
 // Boucle de chargement des urls
-for  ( i  =  0 ;  i  <  urlsToGet . length ;  i ++ )  {
-	getData ( i ) ;
+for (i = 0; i < urlsToGet.length; i++) {
+	getData(i);
 }
